413632fd6869efa974a0a71e4895e6e7ca5856e4
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2022-05-24 17:46:23 +0100

    test(private_key): verify using calculations

--- spec/ecc/private_key_spec.rb
+++ spec/ecc/private_key_spec.rb
@@ -9,8 +9,21 @@ RSpec.describe ECC::PrivateKey do
     let(:z) { rand(Secp256k1Constants::N) }
     let(:signature) { private_key.sign(z) }
 
-    it 'generates a valid signature' do
-      expect(private_key.point.verify(z, signature)).to be true
+    context 'validation using point method verify' do
+      it 'generates a valid signature' do
+        expect(private_key.point.verify(z, signature)).to be true
+      end
+    end
+
+    context 'validation using analytical calculations' do
+      let(:s_inv) { signature.s.pow(Secp256k1Constants::N - 2, Secp256k1Constants::N) }
+      let(:u) { z * s_inv % Secp256k1Constants::N }
+      let(:v) { signature.r * s_inv % Secp256k1Constants::N }
+      let(:random_target) { (ECC::S256Point::G * u + private_key.point * v).x.num }
+
+      it 'generates a valid signature' do
+        expect(signature.r).to eq random_target
+      end
     end
   end
 end
