8f01b47788223cb568b701add774ae924b95dff6
Javier G. Montoya S <jgmontoya@pm.me>
2022-05-29 22:51:50 -0400

    feat(lib/ecc/s256_point): add sec, hash160, address and self.parse methods

--- lib/ecc/s256_point.rb
+++ lib/ecc/s256_point.rb
@@ -1,9 +1,14 @@
 require_relative 's256_field'
 require_relative 'point'
 require_relative 'secp256k1_constants'
+require_relative '../encoding_helper'
+require_relative '../hash_helper'
 
 module ECC
   class S256Point < Point
+    include EncodingHelper
+    extend EncodingHelper
+
     def initialize(x, y, _a = 0, _b = 7)
       a = S256Field.new(Secp256k1Constants::A)
       b = S256Field.new(Secp256k1Constants::B)
@@ -29,10 +34,6 @@ module ECC
       super(coef)
     end
 
-    def self.G
-      S256Point.new(Secp256k1Constants::G_X, Secp256k1Constants::G_Y)
-    end
-
     def verify(z, signature)
       s_inv = signature.s.pow(Secp256k1Constants::N - 2, Secp256k1Constants::N)
       u = z * s_inv % Secp256k1Constants::N
@@ -40,5 +41,47 @@ module ECC
       random_target = (u * self.class::G + v * self).x.num
       random_target == signature.r
     end
+
+    def sec(compressed: true)
+      if compressed
+        prefix = y.num.even? ? to_bytes(2, 1, 'big') : to_bytes(3, 1, 'big')
+        return prefix + to_bytes(x.num, 32, 'big')
+      end
+
+      to_bytes(4, 1, 'big') + to_bytes(x.num, 32, 'big') + to_bytes(y.num, 32, 'big')
+    end
+
+    def hash160(compressed: true)
+      HashHelper.hash160(sec(compressed: compressed))
+    end
+
+    def address(compressed: true, testnet: false)
+      prefix = testnet ? "\x6f" : "\x00"
+      encode_base58_checksum(prefix + hash160(compressed: compressed))
+    end
+
+    def self.parse(sec_bin)
+      return parse_uncompressed(sec_bin) if sec_bin[0] == "\x04"
+
+      x = S256Field.new(from_bytes(sec_bin[1..], 'big'))
+      alpha = x**3 + S256Field.new(Secp256k1Constants::B)
+      beta = alpha.sqrt
+      if beta.num.even?
+        even_beta = beta
+        odd_beta = S256Field.new(Secp256k1Constants::P - beta.num)
+      else
+        even_beta = S256Field.new(Secp256k1Constants::P - beta.num)
+        odd_beta = beta
+      end
+      sec_bin[0] == "\x02" ? new(x, even_beta) : new(x, odd_beta)
+    end
+
+    def self.parse_uncompressed(sec_bin)
+      x = from_bytes(sec_bin.slice(1, 32), 'big')
+      y = from_bytes(sec_bin.slice(33, 32), 'big')
+      new(x, y)
+    end
+
+    private_class_method :parse_uncompressed
   end
 end
--- spec/ecc/s256_point_spec.rb
+++ spec/ecc/s256_point_spec.rb
@@ -3,6 +3,9 @@ require 'ecc/s256_field'
 require 'ecc/field_element'
 require 'ecc/signature'
 require 'ecc/secp256k1_constants'
+require 'ecc/private_key'
+require 'encoding_helper'
+require 'hash_helper'
 
 RSpec.describe ECC::S256Point do
   describe 'init' do
@@ -67,6 +70,69 @@ RSpec.describe ECC::S256Point do
       it 'returns false' do
         expect(point.verify(z_modified, signature)).to be false
       end
+    end
+  end
+
+  describe '#sec' do
+    it 'returns the binary version of the uncompressed SEC format' do
+      pubkey = ECC::PrivateKey.new(5000).point
+      sec_bytes_hex = "04ffe558e388852f0120e46af2d1b370f85854a8eb0841811ece0e3e"\
+                      "03d282d57c315dc72890a4f10a1481c031b03b351b0dc79901ca18a00cf009dbdb157a1d10"
+      expect(pubkey.sec(compressed: false).unpack1('H*')).to eq(sec_bytes_hex)
+    end
+
+    it 'returns the binary version of the compressed SEC format' do
+      pubkey = ECC::PrivateKey.new(5000).point
+      sec_bytes_hex = "02ffe558e388852f0120e46af2d1b370f85854a8eb0841811ece0e3e03d282d57c"
+      expect(pubkey.sec(compressed: true).unpack1('H*')).to eq(sec_bytes_hex)
+    end
+  end
+
+  describe '#hash160' do
+    it 'returns the hash160 of the sec encoding' do
+      pubkey = ECC::PrivateKey.new(5000).point
+      hash = HashHelper.hash160(pubkey.sec(compressed: true))
+      expect(pubkey.hash160(compressed: true)).to eq hash
+    end
+  end
+
+  describe '#address' do
+    it 'generates the proper compressed testnet address' do
+      pubkey = ECC::PrivateKey.new(888**3).point
+      address = 'mieaqB68xDCtbUBYFoUNcmZNwk74xcBfTP'
+      expect(pubkey.address(compressed: true, testnet: true)).to eq address
+    end
+
+    it 'generates the proper compressed mainnet address' do
+      pubkey = ECC::PrivateKey.new(888**3).point
+      address = '148dY81A9BmdpMhvYEVznrM45kWN32vSCN'
+      expect(pubkey.address(compressed: true, testnet: false)).to eq address
+    end
+
+    it 'generates the proper uncompressed testnet address' do
+      pubkey = ECC::PrivateKey.new(4242424242).point
+      address = 'mgY3bVusRUL6ZB2Ss999CSrGVbdRwVpM8s'
+      expect(pubkey.address(compressed: false, testnet: true)).to eq address
+    end
+
+    it 'generates the proper uncompressed mainnet address' do
+      pubkey = ECC::PrivateKey.new(4242424242).point
+      address = '1226JSptcStqn4Yq9aAmNXdwdc2ixuH9nb'
+      expect(pubkey.address(compressed: false, testnet: false)).to eq address
+    end
+  end
+
+  describe '#self.parse' do
+    it 'returns a Point object from a uncompressed SEC binary' do
+      point = ECC::PrivateKey.new(69420).point
+      sec_bytes = point.sec(compressed: false)
+      expect(described_class.parse(sec_bytes)).to eq point
+    end
+
+    it 'returns a Point object from a compressed SEC binary' do
+      point = ECC::PrivateKey.new(69420).point
+      sec_bytes = point.sec(compressed: true)
+      expect(described_class.parse(sec_bytes)).to eq point
     end
   end
 end
